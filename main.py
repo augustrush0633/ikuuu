c69a6fd6313b77c7dd2f56099cc95a2191383d92
augustrush0633 <65332635+augustrush0633@users.noreply.github.com>
2025-08-12 12:27:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ def checkin(cookie):
             print_with_time(f"签到成功: {data.get('msg', '获得奖励')}", "SUCCESS")
              # 进行推送
             if SCKEY != '':
-                    push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, data)
+                    push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, msg)
                     requests.post(url=push_url)
                     print('推送成功')            
             return True
@@ -243,7 +243,7 @@ def checkin(cookie):
             print_with_time(f"今日已签到: {data.get('msg', '请明天再来')}", "WARNING")
             # 进行推送
             if SCKEY != '':
-                    push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, data)
+                    push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, msg)
                     requests.post(url=push_url)
                     print('推送成功')          
             return True
@@ -251,7 +251,7 @@ def checkin(cookie):
             print_with_time(f"签到失败: {data.get('msg', '未知错误')}", "ERROR")
             # 进行推送
             if SCKEY != '':
-                    push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, data)
+                    push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, msg)
                     requests.post(url=push_url)
                     print('推送成功')      
             return False
